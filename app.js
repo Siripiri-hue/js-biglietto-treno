3e0fdf0eed54c3b28631eb24b5fa376a1452babf
Siripiri-hue <daemonprocellae@live.it>
2022-02-04 15:51:51 +0100

    Aggiunte operazioni base

---
+++ app.js
@@ -0,0 +1,25 @@
+// chiedo quanti km si vogliono percorrere
+let totKM = prompt("Ciao! Inserisci il numero dei km che vuoi percorrere");
+console.log(totKM);
+
+// chiedo l'età del passeggero
+let userAge = prompt("Inserisci ora l'età della persona che vuole viaggiare");
+console.log(userAge);
+
+// calcolo costo del biglietto
+let ticketPrice = totKM * 0.21;
+console.log(ticketPrice);
+
+//verifico se la persona può usufruire di qualche sconto
+if (userAge < 18) 
+{ 
+    ticketPrice = ticketPrice*80/100;
+}
+else if (userAge >= 65)
+{
+    ticketPrice = ticketPrice*60/100;
+}
+
+let message = `Il costo del biglietto è di ${ticketPrice}€`;
+console.log(message);
+alert(message);
